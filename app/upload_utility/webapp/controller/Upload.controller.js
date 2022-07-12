44984b32343cbc1155063ac77b7c6331e06ce12f
ran hassid <ran.hassid@gmail.com>
2022-07-12 15:35:18 +0000

    upload utils, add csv to json delimiter

--- app/upload_utility/webapp/controller/Upload.controller.js
+++ app/upload_utility/webapp/controller/Upload.controller.js
@@ -16,10 +16,60 @@ sap.ui.define(
        */
       onInit: function () {
         // Model used to manipulate control states
-        var oViewModel = new JSONModel({});
-        this.setModel(oViewModel, "worklistView");
+        var oViewModel = new JSONModel({
+          uploadTypeIndex: -1,
+          selectedFile: "",
+          uploading: false,
+        });
+        this.setModel(oViewModel, "uploadView");
       },
-      onUpload: function () {},
+      onUpload: function () {
+        if (!this.oSelectedFile) {
+          return;
+        }
+
+        // use file reader to read the file contetn
+        var reader = new FileReader();
+        reader.readAsText(this.oSelectedFile, "UTF-8");
+
+        var self = this;
+
+        reader.onload = function (event) {
+          var records = self._csvToJSON(event.target.result);
+          self._uploadRecords(records);
+        };
+      },
+      _uploadRecords: function (records) {
+        // this.getModel("uploadView").setProperty("/uploading", true);
+
+        var oDataListBinding = this.getModel().bindList(
+          "/triprecord",
+          null,
+          null,
+          null,
+          {}
+        );
+
+        var oRecord = this._toLowerKeys(records[0]);
+        oRecord.actgatetime = parseInt(oRecord.actgatetime);
+        oRecord.actualflyingdur = parseInt(oRecord.actualflyingdur);
+        oRecord.estdeptdateutc = "2021-10-14";
+        oRecord.estarrdate = "2021-10-14";
+        oRecord.planblocktime = parseInt(oRecord.planblocktime);
+        oRecord.flight_tm = parseInt(oRecord.flight_tm);
+        oRecord.onblockdate = "2021-10-14";
+        oRecord.onblocktime = "10:30:00";
+        oRecord.offblockdate = "2021-10-14";
+        oRecord.taxi_out_time = parseInt(oRecord.taxi_out_time);
+
+        oDataListBinding.create(oRecord, false, false, false);
+      },
+      _toLowerKeys: function (obj) {
+        return Object.keys(obj).reduce((accumulator, key) => {
+          accumulator[key.toLowerCase()] = obj[key];
+          return accumulator;
+        }, {});
+      },
       onFileChange: function (oEvent) {
         // read the selected file
         var oFile = oEvent.getParameter("files")[0];
@@ -28,33 +78,18 @@ sap.ui.define(
           return;
         }
 
-        // use file reader to read the file contetn
-        var reader = new FileReader();
-        reader.readAsText(oFile, "UTF-8");
-
-        var self = this;
-
-        reader.onload = function (event) {
-          var csvJSON = self._csvToJSON(event.target.result);
-          debugger;
-          var jsonObj = JSON.parse(csvJSON);
-          debugger;
-        };
+        this.oSelectedFile = oFile;
       },
-      _csvToJSON: function (csv) {
+      _csvToJSON: function (csv, delimiter = ",", stringify = false) {
         var lines = csv.split("\n");
 
         var result = [];
 
-        // NOTE: If your columns contain commas in their values, you'll need
-        // to deal with those before doing the next step
-        // (you might convert them to &&& or something, then covert them back later)
-        // jsfiddle showing the issue https://jsfiddle.net/
-        var headers = lines[0].split(",");
+        var headers = lines[0].split(delimiter);
 
         for (var i = 1; i < lines.length; i++) {
           var obj = {};
-          var currentline = lines[i].split(",");
+          var currentline = lines[i].split(delimiter);
 
           for (var j = 0; j < headers.length; j++) {
             obj[headers[j]] = currentline[j];
@@ -63,8 +98,7 @@ sap.ui.define(
           result.push(obj);
         }
 
-        //return result; //JavaScript object
-        return JSON.stringify(result); //JSON
+        return stringify ? JSON.stringify(result) : result;
       },
     });
   }
